c43b708c007ba63909a006e2ecf8ccd3cff6ea62
Harun Kaygusuz <151049377+harunkaygusuz@users.noreply.github.com>
2024-02-06 20:35:37 +0300

    tobeto odev ve projeler
    java dilinde tutulan notları ve yapılan ödevleri içeriyor

---
+++ intro/veriables.java
@@ -0,0 +1,36 @@
+package intro;
+
+public class veriables {
+
+	public static void main(String[] args) {
+		
+		String title = "Mühendis";
+		
+		System.out.println(title); 
+		System.out.println(title); 
+		System.out.println(title); 
+		System.out.println(title); 
+		System.out.println(title); 
+		System.out.println(title); 
+		
+		int number1  = 150;
+		int number2 = 100;
+		
+		
+		double ondalıklı = 29.99 ;  
+		System.out.println(ondalıklı);
+		
+		var total2 = number1 + number2 ;// var bütün veri tiplerini kapsar
+		// double total3 = number1 + ondalıklı; ondalıklı sayılar için kullanılır
+		
+
+		boolean isTrue = true ;
+		
+		// char gender = 'a' ;    tek karakterlik değişkenler için kullanılır
+		
+		
+		
+		
+	}
+
+}
